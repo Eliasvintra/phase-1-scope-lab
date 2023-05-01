193e61f4df8fff369148ab69ce535a73b4d75b6a
Eliasvintra <rbnvintra.ke@gmail.com>
2023-05-01 22:27:06 +0300

    Another commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+
+// declare customerName to be bob in global scope
+customerName = 'bob';
+
+function upperCaseCustomerName() {
+  // modifies the customerName variable
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  // setBestCustomer
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  // overwrites the best customer
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value';
+}
+
+
